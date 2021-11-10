09d75263dbd3c004404bbcf3cea78ea758376f6f
PSYKN <74043034+PSYKN@users.noreply.github.com>
2021-11-09 19:20:35 -0700

    Transfer HTML to template

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,8 +18,13 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Use(NoSurve)
 	mux.Use(SessionLoad)
 
-	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/majors-suit", handlers.Repo.Majors)
+	mux.Get("/make-reservation", handlers.Repo.Reservations)
+	mux.Get("/search-availability", handlers.Repo.SearchAvailability)
+	mux.Get("/contact", handlers.Repo.Contact)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
--- pkd/handlers/handlers.go
+++ pkd/handlers/handlers.go
@@ -28,21 +28,30 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	remoteIP := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 	render.RenderTemplate(w, "home.page.tmpl", &modules.TemplateData{})
 }
 
 // About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	//
-	stringMap := make(map[string]string)
-	stringMap["test"] = "Hello Again"
+	render.RenderTemplate(w, "about.page.tmpl", &modules.TemplateData{})
+}
 
-	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
-	stringMap["remote_ip"] = remoteIP
+func (m *Repository) Reservations(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.tmpl", &modules.TemplateData{})
+}
 
-	render.RenderTemplate(w, "about.page.tmpl", &modules.TemplateData{
-		StringMap: stringMap,
-	})
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.tmpl", &modules.TemplateData{})
+}
+
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.tmpl", &modules.TemplateData{})
+}
+
+func (m *Repository) SearchAvailability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.tmpl", &modules.TemplateData{})
+}
+
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.tmpl", &modules.TemplateData{})
 }
