6e3d39ee1a2005396844f9b206559b5fff26a0dd
PSYKN <74043034+PSYKN@users.noreply.github.com>
2021-11-21 15:08:35 -0700

    sql seeding

---
+++ migrations/20211121220654_create_seed_restrictions_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+('Reservation','2020-11-18 00:00:00.000','2020-11-18 00:00:00.000'),
+('Owner Block','2021-11-21 00:00:00.000','2021-11-21 00:00:00.000');
