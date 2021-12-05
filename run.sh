215f26c4a5f3745d82e312c789b9bd09c73c69f2
PSYKN <74043034+PSYKN@users.noreply.github.com>
2021-12-05 16:14:46 -0700

    admin static compressed clean

--- run.sh
+++
@@ -1,4 +0,0 @@
-go build -o bookings cmd/web/*.go
-
-#host = localhost port = 5432 dbname = bookings user = postgres password = 123456"
-./bookings -dbhost=localhost dbport=5432 -dbname=booking -dbuser=postgres -dbpass=123456 -cache=false -production=false
